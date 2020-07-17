f3d55893e55818813685408592a06ffd763c1c3a
laureanray <laureanraybahala@gmail.com>
2020-07-17 11:24:10 +0800

    initial commit

---
+++ src/main/java/tech/laureanray/utangswebapi/UtangsWebApiApplication.java
@@ -0,0 +1,13 @@
+package tech.laureanray.utangswebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtangsWebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtangsWebApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/laureanray/utangswebapi/UtangsWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.laureanray.utangswebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtangsWebApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
